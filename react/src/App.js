036285cd3200f5d93831b5086e045dec9d05eabc
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-03 14:27:09 +0300

    FE//Creating admin paths

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import AddCategory from "./pages/Admin/AddCategory";
+import AddPlace from "./pages/Admin/AddPlace";
+import Login from "./pages/Admin/Login";
+
 function App() {
   return (
-    <div>HELLO WORLD!</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="admin/Login" element={<Login/>}></Route>
+        <Route path="admin/AddCategory" element={<AddCategory/>}></Route>
+        <Route path="admin/AddPlace" element={<AddPlace/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
