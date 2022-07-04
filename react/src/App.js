09e35bdb64036f05585aeee9292d0e3507d711d5
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-07-04 08:29:26 +0300

    FE//Creating TopNavbar component

--- react/src/App.js
+++ react/src/App.js
@@ -2,10 +2,12 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import AddCategory from "./pages/Admin/AddCategory";
 import AddPlace from "./pages/Admin/AddPlace";
 import Login from "./pages/Admin/Login";
+import TopNavbar from "./components/TopNavbar";
 
 function App() {
   return (
     <BrowserRouter>
+    <TopNavbar/>
       <Routes>
         <Route path="admin/Login" element={<Login/>}></Route>
         <Route path="admin/AddCategory" element={<AddCategory/>}></Route>
---
+++ react/src/components/TopNavbar.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+const TopNavbar = () => {
+
+    return (
+        <>
+            <div className="top-bar">
+                <img src="assets/airbnb-logo-vector.jpg" alt="logo" />
+                <button type="button" className="logo">Admin Login</button>
+            </div>
+        </>
+    )
+};
+
+export default TopNavbar;
